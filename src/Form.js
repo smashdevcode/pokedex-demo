1c025f171f89ba3d9a73551c14be7f8f4b903dad
James Churchill <james@smashdev.com>
2022-01-10 15:52:10 -0800

    Add code comments

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,9 @@ function Form({ pokemon, setPokemon }) {
     const [searchTerm, setSearchTerm] = useState("");
     const [allPokemon, setAllPokemon] = useState([]);
 
+    // When the component loads initially,
+    // make a copy of the passed in pokemon array
+    // if and only if the copy of the array is empty.
     useEffect(() => {
         if (allPokemon.length === 0) {
             setAllPokemon(pokemon);
@@ -14,6 +17,8 @@ function Form({ pokemon, setPokemon }) {
     function search(event) {
         event.preventDefault();
 
+        // By using the Array#some() method we can use the same toLowerCase trick
+        // to make our search within an array of values case-insensitive.
         const filteredPokemon = allPokemon.filter(p => 
             p.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
             p.type.some(t => t.toLowerCase().includes(searchTerm.toLowerCase())));
@@ -30,6 +35,7 @@ function Form({ pokemon, setPokemon }) {
         <>
             <form onSubmit={search}>
                 <label htmlFor="searchTerm">Search by Name or Type:</label>
+                {/* Switch to using a controller component so we don't have to access the DOM directly via our JS code. */}
                 <input type="text" name="searchTerm" value={searchTerm} 
                     onChange={(event) => setSearchTerm(event.target.value)}
                     placeholder="Enter Pokemon Name or Type Here" />
